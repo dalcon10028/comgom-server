863593457bb46fd0d6dacd4f4a9ef7fd59131bd1
dalcon10028 <dalcon10280@gmail.com>
2021-08-17 17:09:41 +0000

    refactor: pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,12 +5,7 @@ module.exports = {
       exec_mode: 'cluster',
       instances: 2,
       script: 'npm',
-      args: 'start',
-      env_production: {
-        HOST: '0.0.0.0',
-        PORT: 1337,
-        NODE_ENV: 'production'
-      }
+      args: 'start'
     }
   ]
 }
